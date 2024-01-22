7885d72a07d2be22f812289903a5ef0f9558dd9b
choichanghwan <ckdghks5565@gmail.com>
2024-01-22 13:59:01 +0900

    "first commit"

--- StringCalculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.stringcalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.stringcalculator to javafx.fxml;
-    exports org.example.stringcalculator;
-}
