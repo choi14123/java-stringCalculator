1da3e8876c7a51de49f5242a5744faba34dfa0bc
choichanghwan <ckdghks5565@gmail.com>
2024-01-22 19:40:07 +0900

    test: "RepeatTest 기능 추가"

---
+++ StringCalculator/src/test/java/org/example/stringcalculator/model/RepeatTest.java
@@ -0,0 +1,30 @@
+package org.example.stringcalculator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RepeatTest {
+    @Test
+    @DisplayName("최종 결과 값이 나온다.")
+    void loopTest() {
+        //given
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(5);
+        numbers.add(3);
+        numbers.add(10);
+        numbers.add(2);
+        List<String> operators = new ArrayList<>();
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
+        //when
+        Repeat repeat = new Repeat(numbers, operators, operators.size());
+        //then
+        Assertions.assertThat(repeat.getTotalNumber()).isEqualTo(60);
+    }
+}
