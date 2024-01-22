048a1596f1285580dcbdf17372660569b9de2d42
choichanghwan <ckdghks5565@gmail.com>
2024-01-22 19:39:29 +0900

    feat: "Numbers 기능 추가"

---
+++ StringCalculator/src/main/java/org/example/stringcalculator/model/Numbers.java
@@ -0,0 +1,23 @@
+package org.example.stringcalculator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numbers {
+    private List<Integer> numbers = new ArrayList<>();
+
+    public Numbers(String[] values) {
+        numbers = convertToNumbersList(values);
+    }
+
+    private List<Integer> convertToNumbersList(String[] values) {
+        for (int i = 0; i < values.length; i += 2) {
+            numbers.add(Integer.valueOf(values[i]));
+        }
+        return numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
