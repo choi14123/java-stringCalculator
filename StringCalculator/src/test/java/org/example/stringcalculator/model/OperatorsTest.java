905d3f1c6851aa4c918f42c487b7427ea923ce07
choichanghwan <ckdghks5565@gmail.com>
2024-01-28 21:39:13 +0900

    test: "OperatorsTest"

--- StringCalculator/src/test/java/org/example/stringcalculator/model/OperatorsTest.java
+++ StringCalculator/src/test/java/org/example/stringcalculator/model/OperatorsTest.java
@@ -8,18 +8,18 @@ import org.junit.jupiter.api.Test;
 
 class OperatorsTest {
     @Test
-    @DisplayName("operators에 문자만 들어가는지 확인한다.")
+    @DisplayName("operators에 문자 '+', '-', '*', '/'만 들어가는지 확인한다.")
     void setOperatorsTest() {
         //given
         String[] values = new String[]{"1", "+", "2", "/", "3"};
         //when
         Operators operators = new Operators(values);
         //then
-        Assertions.assertThat(operators.getOperatorsSize()).isEqualTo(2);
+        Assertions.assertThat(operators.getOperators().size()).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("operators에는 '+', '-', '*', '/' 만 들어가야 한다. 이외의 문자가 입력 되면 예외 발생.")
+    @DisplayName("operators에는 '+', '-', '*', '/' 만 들어가야 한다. 이외의 문자인 a가 입력되면 입력 되면 예외 발생.")
     void validateOperatorTest() {
         //given
         String[] values = new String[]{"1", "a", "2", "/", "3"};
