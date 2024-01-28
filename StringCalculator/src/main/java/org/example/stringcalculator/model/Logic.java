ec709046687459fda6b8ff639b6970eff1a249ae
choichanghwan <ckdghks5565@gmail.com>
2024-01-28 21:39:24 +0900

    refactor: "Logic"

---
+++ StringCalculator/src/main/java/org/example/stringcalculator/model/Logic.java
@@ -0,0 +1,24 @@
+package org.example.stringcalculator.model;
+
+import java.util.List;
+
+public class Logic {
+    private int totalResultNumber;
+
+    public Logic(List<Integer> numbers, List<String> operators, int operatorsSize) {
+        repeatFourBasicOperation(numbers, operators, operatorsSize);
+    }
+
+    private void repeatFourBasicOperation(List<Integer> numbers, List<String> operators, int operatorsSize) {
+        FourBasicOperation fourBasicOperation = new FourBasicOperation();
+        totalResultNumber = fourBasicOperation.fourOperations(numbers.get(0), numbers.get(1), operators.get(0));
+        for (int i = 1; i < operatorsSize; i++) {
+            totalResultNumber = fourBasicOperation.fourOperations(totalResultNumber, numbers.get(i + 1),
+                    operators.get(i));
+        }
+    }
+
+    public int getTotalNumber() {
+        return totalResultNumber;
+    }
+}
