d5c26c579e43fa05ee880c69d2fd7ffa17ea2f07
choichanghwan <ckdghks5565@gmail.com>
2024-01-28 21:47:02 +0900

    test: "FourBasicOperation"

--- StringCalculator/src/main/java/org/example/stringcalculator/model/FourBasicOperation.java
+++ StringCalculator/src/main/java/org/example/stringcalculator/model/FourBasicOperation.java
@@ -1,21 +1,33 @@
 package org.example.stringcalculator.model;
 
 public class FourBasicOperation {
+    private static final String EXCEPTION_MESSAGE = "나누기에는 0이 입력되면 안됩니다.";
 
     public int fourOperations(int number, int number1, String operator) {
         if (operator.equals("+")) {
-            return number + number1;
+            int result = number + number1;
+            return result;
         }
         if (operator.equals("-")) {
-            return number - number1;
+            int result = number - number1;
+            return result;
         }
         if (operator.equals("/")) {
-            return number / number1;
+            validateNumber(number1);
+            int result = number / number1;
+            return result;
         }
         if (operator.equals("*")) {
-            return number * number1;
+            int result = number * number1;
+            return result;
         }
 
         return number;
+    }
+
+    private void validateNumber(int number1) {
+        if (number1 == 0) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
     }
 }
