553f0312ebc79b723fe91a225f9b2d5dd2cf3e46
choichanghwan <ckdghks5565@gmail.com>
2024-01-28 21:39:00 +0900

    test: "LogicTest"

---
+++ StringCalculator/src/test/java/org/example/stringcalculator/model/LogicTest.java
@@ -0,0 +1,21 @@
+package org.example.stringcalculator.model;
+
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LogicTest {
+    @Test
+    @DisplayName("사칙연산 후 최종 값이 나온다.")
+    void loopTest() {
+        //given
+        List<Integer> numbers = Arrays.asList(10, 5, 3, 10, 2);
+        List<String> operators = Arrays.asList("+", "-", "*", "/");
+        //when
+        Logic logic = new Logic(numbers, operators, operators.size());
+        //then
+        Assertions.assertThat(logic.getTotalNumber()).isEqualTo(60);
+    }
+}
