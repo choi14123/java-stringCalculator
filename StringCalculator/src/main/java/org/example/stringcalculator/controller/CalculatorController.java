0be9c912d92064f2bf700e4f0fce0fd7d12511a4
choichanghwan <ckdghks5565@gmail.com>
2024-01-28 22:02:10 +0900

    refactor: "CalculatorController"

--- StringCalculator/src/main/java/org/example/stringcalculator/controller/CalculatorController.java
+++ StringCalculator/src/main/java/org/example/stringcalculator/controller/CalculatorController.java
@@ -7,20 +7,22 @@ import org.example.stringcalculator.view.InputView;
 import org.example.stringcalculator.view.OutputView;
 
 public class CalculatorController {
+
+    private final InputView inputView;
+    private final Logic logic;
     private final OutputView outputView;
-    private final Numbers numbers;
-    private final Operators operators;
 
     public CalculatorController() {
-        InputView inputView = new InputView();
-        String[] values = inputView.array();
-        this.numbers = new Numbers(values);
-        this.operators = new Operators(values);
+        this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.logic = new Logic();
     }
 
     public void start() {
-        Logic logic = new Logic(numbers.getNumbers(), operators.getOperators(), operators.getOperators().size());
+        String[] values = inputView.array();
+        Numbers numbers = new Numbers(values);
+        Operators operators = new Operators(values);
+        logic.repeatFourBasicOperation(numbers.getNumbers(), operators.getOperators(), operators.getOperators().size());
         outputView.printResult(logic.getTotalNumber());
     }
 }
