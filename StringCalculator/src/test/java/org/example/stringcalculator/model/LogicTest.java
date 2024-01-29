3050d0551103edf0939c925934177d35cd5d7ca4
choichanghwan <ckdghks5565@gmail.com>
2024-01-29 11:13:57 +0900

    test: "LogicTest"

--- StringCalculator/src/test/java/org/example/stringcalculator/model/LogicTest.java
+++ StringCalculator/src/test/java/org/example/stringcalculator/model/LogicTest.java
@@ -1,6 +1,6 @@
 package org.example.stringcalculator.model;
 
-import java.util.Arrays;
+
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -8,15 +8,16 @@ import org.junit.jupiter.api.Test;
 
 class LogicTest {
     @Test
-    @DisplayName("사칙연산 후 최종 값이 나온다.")
+    @DisplayName("사칙연산 후 최종 값인 60이 나온다.")
     void repeatFourBasicOperationTest() {
         //given
-        List<Integer> numbers = Arrays.asList(10, 5, 3, 10, 2);
-        List<String> operators = Arrays.asList("+", "-", "*", "/");
+        int totalNumber = 60;
+        List<Integer> numbers = List.of(10, 5, 3, 10, 2);
+        List<String> operators = List.of("+", "-", "*", "/");
         //when
         Logic logic = new Logic();
         logic.repeatFourBasicOperation(numbers, operators, operators.size());
         //then
-        Assertions.assertThat(logic.getTotalNumber()).isEqualTo(60);
+        Assertions.assertThat(logic.getTotalNumber()).isEqualTo(totalNumber);
     }
 }
