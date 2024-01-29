8bf0442208192e938f77d72655465e91cc559db6
choichanghwan <ckdghks5565@gmail.com>
2024-01-29 11:14:04 +0900

    test: "NumbersTest"

--- StringCalculator/src/test/java/org/example/stringcalculator/model/NumbersTest.java
+++ StringCalculator/src/test/java/org/example/stringcalculator/model/NumbersTest.java
@@ -1,5 +1,6 @@
 package org.example.stringcalculator.model;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,15 +8,14 @@ import org.junit.jupiter.api.Test;
 class NumbersTest {
 
     @Test
-    @DisplayName("numbers에 숫자가 저장 및 반환 되는지 확인한다.")
+    @DisplayName("객체에 숫자가 저장 및 반환 되는지 확인한다.")
     void setNumbersTest() {
         //given
         String[] values = new String[]{"1", "+", "2", "/", "3"};
+        List<Integer> numbersList = List.of(1, 2, 3);
         //when
         Numbers numbers = new Numbers(values);
         //then
-        Assertions.assertThat(numbers.getNumbers().get(0)).isEqualTo(1);
-        Assertions.assertThat(numbers.getNumbers().get(1)).isEqualTo(2);
-        Assertions.assertThat(numbers.getNumbers().get(2)).isEqualTo(3);
+        Assertions.assertThat(numbers.getNumbers()).isEqualTo(numbersList);
     }
 }
