3874e7c28e4a335cba3e526231dc4b7414e6c37d
choichanghwan <ckdghks5565@gmail.com>
2024-01-30 10:31:49 +0900

    refactor: "Logic"

--- StringCalculator/src/main/java/org/example/stringcalculator/model/Logic.java
+++ StringCalculator/src/main/java/org/example/stringcalculator/model/Logic.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Logic {
+    private final FourBasicOperation fourBasicOperation = new FourBasicOperation();
+
     private int totalResultNumber;
-    private FourBasicOperation fourBasicOperation = new FourBasicOperation();
 
     public void repeatFourBasicOperation(List<Integer> numbers, List<String> operators, int operatorsSize) {
-        totalResultNumber = fourBasicOperation.getTotalResultNumber(numbers.get(0), numbers.get(1),
-                operators.get(0));
-        IntStream.range(1, operatorsSize)
-                .forEach(i -> totalResultNumber = fourBasicOperation.getTotalResultNumber(totalResultNumber,
-                        numbers.get(i + 1), operators.get(i)));
+        totalResultNumber = fourBasicOperation.getTotalResultNumber(numbers.get(0), numbers.get(1), operators.get(0));
+        IntStream.range(1, operatorsSize).forEach(
+                i -> totalResultNumber = fourBasicOperation.getTotalResultNumber(totalResultNumber, numbers.get(i + 1),
+                        operators.get(i)));
     }
 
     public int getTotalNumber() {
