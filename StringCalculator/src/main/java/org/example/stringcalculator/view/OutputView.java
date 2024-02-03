92d84e752b36a4f250e9f0368d41e725739b763e
choichanghwan <ckdghks5565@gmail.com>
2024-02-03 15:41:25 +0900

    refactor: "OutputView"

---
+++ StringCalculator/src/main/java/org/example/stringcalculator/view/OutputView.java
@@ -0,0 +1,7 @@
+package org.example.stringcalculator.view;
+
+public class OutputView {
+    public void totalResultNumber(int totalResultNumber) {
+        System.out.println(totalResultNumber);
+    }
+}
