06c4f95bdd25b387c3e2b9a0baf4c8c2c989cc5d
choichanghwan <ckdghks5565@gmail.com>
2024-02-07 18:51:50 +0900

    refactor: "StringCalculatorController"

--- StringCalculator/src/main/java/org/example/stringcalculator/controller/StringCalculatorController.java
+++ StringCalculator/src/main/java/org/example/stringcalculator/controller/StringCalculatorController.java
@@ -1,6 +1,8 @@
 package org.example.stringcalculator.controller;
 
-import org.example.stringcalculator.model.FourBasicOperation;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.example.stringcalculator.model.CalculatorMapping;
 import org.example.stringcalculator.model.Numbers;
 import org.example.stringcalculator.model.Operators;
 import org.example.stringcalculator.view.InputView;
@@ -9,12 +11,10 @@ import org.example.stringcalculator.view.OutputView;
 public class StringCalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
-    private final FourBasicOperation fourBasicOperation;
 
     public StringCalculatorController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
-        this.fourBasicOperation = new FourBasicOperation();
     }
 
     public void start() {
@@ -23,9 +23,19 @@ public class StringCalculatorController {
         Numbers numbers = new Numbers(values);
         Operators operators = new Operators(values);
 
-        int calculationResult = fourBasicOperation.repeatFourBasicOperation(numbers.getNumbers(),
-                operators.getOperators(), operators.getOperators().size());
-
+        int calculationResult = repeatFourBasicOperation(numbers.getNumbers(), operators.getOperators(),
+                operators.getOperators().size());
         outputView.totalResultNumber(calculationResult);
+    }
+
+    private int repeatFourBasicOperation(List<Integer> numbers, List<String> operators, int operatorsSize) {
+        int number = numbers.get(0);
+        int number1 = numbers.get(1);
+        String operator = operators.get(0);
+
+        int calculationResult = CalculatorMapping.getCalculator(operator).calculate(number, number1);
+
+        return IntStream.range(1, operatorsSize).map(i -> CalculatorMapping.getCalculator(operators.get(i))
+                .calculate(calculationResult, numbers.get(i + 1))).findFirst().orElse(calculationResult);
     }
 }
